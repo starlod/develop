cda366b6165d784e8ba151c6985e370984022dcf
starlod <starlod.ggl@gmail.com>
2016-11-02 23:27:30 +0900

    apacheのcookbookを作成

---
+++ chef-repo/site-cookbooks/apache/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: httpd24
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'yum::default'
+include_recipe 'yum-ius::default'
+
+%W[
+  httpd
+  httpd-devel
+  httpd-manual
+].each do |pkg|
+  package pkg do
+    action [ :install, :upgrade ]
+    options "--enablerepo=ius"
+  end
+end
+
+service "httpd" do
+  action [:start, :enable]
+  supports :start => true, :status => true, :restart => true, :reload => true
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+  notifies :reload, 'service[httpd]'
+end
